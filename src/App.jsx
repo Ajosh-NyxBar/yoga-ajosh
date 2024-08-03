d8728ff02c7e4f43b314ead439dea381386687ea
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-08-03 09:29:17 +0700

    adding setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import styles from './style'
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Hero, Navbar, Stats, Testimonials } from './components'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='bg-primary text-white overflow-hidden'>
+      <div className={`${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
     </div>
   )
 }
