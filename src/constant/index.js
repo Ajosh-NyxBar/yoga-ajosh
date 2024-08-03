b763fb4499060536636bec025c71dd018e2f3ec1
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-08-03 15:22:48 +0700

    add env

---
+++ src/constant/index.js
@@ -0,0 +1,46 @@
+import {
+  lunar,
+  bill,
+  aline,
+  card,
+  ford,
+  dropbox,
+  logo,
+  quotes,
+  women,
+  medi,
+  mind,
+  health,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Classes",
+  },
+  {
+    id: "product",
+    title: "App",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
